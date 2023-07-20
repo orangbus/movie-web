539ec156e2a9cf0c06defcb51040fd7caa8a3cba
orangbus <1353727504@qq.com>
2023-07-20 14:21:35 +0800

    添加vercel跨域支持

--- api/proxy.js
+++ api/proxy.js
@@ -6,7 +6,7 @@ module.exports = (req, res) => {
 	let target = ''
 	// 代理目标地址
 	// xxxxx 替换为你跨域请求的服务器 如： http://baidu.com
-	if (req.url.startsWith('/api')) { //这里使用/api可能会与vercel serverless 的 api 路径冲突，根据接口进行调整
+	if (req.url.startsWith('/')) { //这里使用/api可能会与vercel serverless 的 api 路径冲突，根据接口进行调整
 		target = 'http://162.14.72.65:8090' //这里就是在vite中配置的一样
 	}
 	// 创建代理对象并转发请求
@@ -15,7 +15,7 @@ module.exports = (req, res) => {
 		changeOrigin: true,
 		pathRewrite: {
 			// 通过路径重写，去除请求路径中的 `/api`
-			'^/api/': '/'
+			'^/': '/'
 		}
 	})(req, res)
 }
